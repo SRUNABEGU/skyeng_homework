4af3fcb94526ca593309d6a22c407997183e5690
ruslan <ruslanpetryakov@yandex.ru>
2025-09-16 02:53:39 +0800

    обновлён декоратор, обновлены тесты

--- src/decorators.py
+++ src/decorators.py
@@ -1,6 +1,9 @@
+from functools import wraps
+
 
 def log(filename=None):
     def inner(function):
+        @wraps(function)
         def wrapper(*args, **kwargs):
             try:
                 result = function(*args, **kwargs)
@@ -8,12 +11,18 @@ def log(filename=None):
                 if filename:
                     with open(filename, 'a') as file:
                         file.write(f'{report}\n')
-                return report
+                print(report)
             except Exception as e:
-                report = f'{function.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}'
+                report = f"{function.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}"
                 if filename:
                     with open(filename, 'a') as file:
                         file.write(f'{report}\n')
-                return report
+                print(report)
         return wrapper
     return inner
+
+@log('log')
+def my_function(x, y):
+    return x + y
+
+my_function('10', 20)
