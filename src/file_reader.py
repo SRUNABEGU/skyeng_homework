4fdee474754237bdd800be39ea80bd8f15df55f8
ruslan <ruslanpetryakov@yandex.ru>
2025-09-30 18:40:58 +0800

    Исправлены баги, обновлён отчёт о покрытии тестами, для работы с файлами теперь используются абсолютные пути

--- src/file_reader.py
+++ src/file_reader.py
@@ -1,9 +1,13 @@
 import csv
+import os
 
 import pandas as pd
 
+ABSPATH_TO_CSV = os.path.join(os.path.dirname(__file__), "..", "data", "transactions.csv")
+ABSPATH_TO_XLSX = os.path.join(os.path.dirname(__file__), "..", "data", "transactions_excel.xlsx")
 
-def csv_reader(path: str = "../data/transactions.csv") -> list:
+
+def csv_reader(path: str = ABSPATH_TO_CSV) -> list:
     """
     читает csv файл
     :param path: путь к файлу
@@ -17,7 +21,7 @@ def csv_reader(path: str = "../data/transactions.csv") -> list:
     return result
 
 
-def xlsx_reader(path: str = "../data/transactions_excel.xlsx") -> list:
+def xlsx_reader(path: str = ABSPATH_TO_XLSX) -> list:
     """
     читает excel файл
     :param path: путь к файлу
@@ -26,7 +30,6 @@ def xlsx_reader(path: str = "../data/transactions_excel.xlsx") -> list:
     df = pd.read_excel(path)
     result = df.to_dict("records")
     return result
-
 
 # print(csv_reader())
 # print(xlsx_reader())
